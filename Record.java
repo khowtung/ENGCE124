a3b59d64e11f5dd32abb08980a610221357caca9
khowtung <128928645+khowtung@users.noreply.github.com>
2025-07-20 12:38:20 +0700

    Create Record.java

---
+++ Record.java
@@ -0,0 +1,15 @@
+public class Record {
+    String name;
+    int no;
+    String date;
+    String time;
+    String id;
+
+    public Record(String name, int no, String date, String time, String id) {
+        this.name = name;
+        this.no = no;
+        this.date = date;
+        this.time = time;
+        this.id = id;
+    }
+}
